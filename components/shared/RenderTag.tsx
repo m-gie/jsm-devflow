0df9e9dfe4ddbe27275eeaf1bcd11b194d20571a
m.glowacki <m.glowacki@smsnet.pl>
2024-04-15 14:44:50 +0200

    Fix RenderTag

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -22,7 +22,9 @@ const RenderTag = ({
       <Badge className="subtle-medium background-light800_dark300 text-dark400_light500 rounded-md border-none px-4 py-2 uppercase">
         {name}
       </Badge>
-      {count && <p className="small-medium text-dark500_light700">{count}</p>}
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{count}</p>
+      )}
     </Link>
   );
 };
