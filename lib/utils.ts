2383371c1e6c4ad109d26fe6cad90cfd7296e14d
Cienias <cieja0@gmail.com>
2024-05-30 15:01:59 +0200

    Add badges functionality

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import qs from "query-string";
+import { BADGE_CRITERIA } from "@/constants";
+import { BadgeCounts } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -86,4 +88,33 @@ export const removeKeysFromQuery = ({
     },
     { skipNull: true }
   );
+};
+
+interface BadgeParam {
+  criteria: {
+    type: keyof typeof BADGE_CRITERIA;
+    count: number;
+  }[];
+}
+
+export const assignBadges = (params: BadgeParam) => {
+  const badgeCounts: BadgeCounts = {
+    GOLD: 0,
+    SILVER: 0,
+    BRONZE: 0,
+  };
+
+  const { criteria } = params;
+
+  for (const criterium of criteria) {
+    const { type, count } = criterium;
+    const badgeCriteria: any = BADGE_CRITERIA[type];
+
+    Object.keys(badgeCriteria).forEach((level: any) => {
+      if (count >= badgeCriteria[level]) {
+        badgeCounts[level as keyof BadgeCounts] += 1;
+      }
+    });
+  }
+  return badgeCounts;
 };
