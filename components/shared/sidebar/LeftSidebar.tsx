795e1d2e8971e2fea2e1fcabe0a9d1c3cb96cd30
Cienias <cieja0@gmail.com>
2024-05-31 11:00:41 +0200

    Fix hide Profile link on sidebar when user is logged out

--- components/shared/sidebar/LeftSidebar.tsx
+++ components/shared/sidebar/LeftSidebar.tsx
@@ -11,6 +11,7 @@ const LeftSidebar = () => {
   const pathname = usePathname();
   const userId = useAuth();
 
+  console.log("LEFT SIDEBAR", userId.userId);
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
@@ -20,7 +21,7 @@ const LeftSidebar = () => {
             pathname === link.route;
 
           if (link.route === "/profile") {
-            if (userId) {
+            if (userId.userId) {
               link.route = `${link.route}/${userId.userId}`;
             } else {
               return null;
